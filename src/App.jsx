485915deb64bb78ef8bc76d38cea05f074764d3a
Heyyyyyy 😈 <saqlainmalik73@gmail.com>
2025-08-02 04:38:32 +0530

    Import fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import Navbar from "./sections/navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Experiences from "./sections/Experiences";
-import Testimonial from "./sections/Testimonial";
+// import Testimonial from "./sections/Testimonial";
 import Contact from "./sections/Contact";
 import Footer from './sections/Footer';
 
